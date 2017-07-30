278d6b58e90d08e706403faba6a06a9cb9f4fb32
Andrew Li <andrewowenliwang@gmail.com>
2017-07-29 20:08:28 -0700

    Commit message

--- components/ShareShare.js
+++ components/ShareShare.js
@@ -5,89 +5,31 @@ import Data from './Data';
 import HistoryHistory from './HistoryHistory';
 import Home from './Home';
 import Share, {ShareSheet, Button} from 'react-native-share';
+import {
+  shareOnFacebook
+  shareOnTwitter
+} from 'react-native-social-share';
 
-class ShareShare extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false
+tweet : function() {
+  shareOnTwitter ({
+    'text':'Look at my feelings!'
+    'link': ''
+    'imagelink':''
+  },
+  (results) => {
+    console.log(results);
     }
-  }
-
-  onCancel() {
-    console.log("CANCEL")
-    this.setState({visible: false});
-  }
-  onOpen() {
-    console.log("OPEN")
-    this.setState({visible: true});
-  }
-  render() {
-    let shareOptions = {
-      title: "FaceApp",
-      message: "Look at how I'm feeling!",
-      url: "http://facebook.github.io/react-native/",
-    };
-
-    let shareImageBase64 = {
-      title: "FaceApp",
-      message: "Look at how I'm feeling!",
-      url: REACT_ICON,
-  };
-  return (
-    <View style={styles.container}>
-
-    <TouchableOpacity onPress={() => {
-      Share.open(shareImageBase64);
-    }}>
-      <View style={styles.instructions}>
-        <Text>Options</Text>
-      </View>
-    </TouchableOpacity>
-
-    <ShareSheet visible={this.state.visible} onCancel={this.onCancel.bind(this)}>
-      <Button iconSrc={{ uri: TWITTER_ICON }}
-                onPress={()=>{
-            this.onCancel();
-            setTimeout(() => {
-              Share.shareSingle(Object.assign(shareOptions, {
-                "social": "twitter"
-              }));
-            },300);
-        }}>Twitter</Button>
-      <Button iconSrc={{ uri: FACEBOOK_ICON }}
-        onPress={()=>{
-          this.onCancel();
-          setTimeout(() => {
-            Share.shareSingle(Object.assign(shareOptions, {
-              "social": "facebook"
-            }));
-          },300);
-        }}>Facebook</Button>
-      <Button iconSrc={{ uri: EMAIL_ICON }}
-                  onPress={()=>{
-              this.onCancel();
-              setTimeout(() => {
-                Share.shareSingle(Object.assign(shareOptions, {
-                  "social": "email"
-                }));
-              },300);
-          }}>Email</Button>
-      </ShareSheet>
-    </View>
   );
-}
-}
+},
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+facebookShare : function() {
+  shareOnFacebook({
+    'text':'Look at my feelings!'
+    'link':''
+    'imagelink':''
   },
-  instructions: {
-    marginTop: 20,
-    marginBottom: 20,
-  },
-});
+  (results) => {
+    console.log(results);
+    }
+  );
+},
